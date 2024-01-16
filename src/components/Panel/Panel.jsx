2c8386805220f478904c173b1cd4837efb95b553
Ismael <ismagarmon3@gmail.com>
2024-01-16 17:23:01 +0100

    up

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { BarChart } from '@mui/x-charts/BarChart'
+
+
 
 const Panel = () => {
+    const xLabels = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December'
+    ]
+
+    const getTasks = () => {
+      xLabels.forEach( month => {
+        pData.push(100)
+      })
+    }
+
+    
+
+    const pData = []
+    getTasks()
     return (
-      <div className='ctn'>
-        <h2>Panel Component</h2>
+      <div>
+
+      <BarChart
+        width={1000}
+        height={400}
+        series={[
+          { data: pData, label: 'Task amount', id: 'pvId', stack: 'total' },
+        ]}
+        xAxis={[{ data: xLabels, scaleType: 'band' }]}
+      />
       </div>
     );
   };
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
+import Table from 'react-bootstrap/Table'
+
 
 const TaskList = () => {
     return (
       <div className='ctn'>
-        <h2>TaskList Component</h2>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
     );
   };
